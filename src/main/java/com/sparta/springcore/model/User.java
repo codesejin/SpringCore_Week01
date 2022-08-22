0720434e046eca71211ae3d9edc99f9aedb2635e
sejin\admin <seijin0722@naver.com>
2022-08-22 23:48:31 +0900

    entity가 user인데 데이터가 안넘어감 해결은 @Entity (name = "users")로 했으나 이유가 무엇인지 알아봐야함

--- src/main/java/com/sparta/springcore/model/User.java
+++ src/main/java/com/sparta/springcore/model/User.java
@@ -9,7 +9,7 @@ import javax.persistence.*;
 @Setter
 @Getter // get 함수를 일괄적으로 만들어줍니다.
 @NoArgsConstructor // 기본 생성자를 만들어줍니다.
-@Entity // DB 테이블 역할을 합니다.
+@Entity (name = "users")// DB 테이블 역할을 합니다. //왜 users로 세팅해줘야할까? enum데이터 값때문?
 public class User {
 
     // ID가 자동으로 생성 및 증가합니다.
