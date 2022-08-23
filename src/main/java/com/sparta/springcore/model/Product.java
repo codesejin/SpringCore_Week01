b9bf9a193072fb5c4b0dc82346dfd54bf81c74f1
sejin\admin <seijin0722@naver.com>
2022-08-23 09:12:05 +0900

    관심 상품 등록 조회를 위해 product테이블에 userId추가

--- src/main/java/com/sparta/springcore/model/Product.java
+++ src/main/java/com/sparta/springcore/model/Product.java
@@ -7,6 +7,13 @@ import lombok.Setter;
 
 import javax.persistence.*;
 
+import com.sparta.springcore.dto.ProductRequestDto;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
 @Setter
 @Getter // get 함수를 일괄적으로 만들어줍니다.
 @NoArgsConstructor // 기본 생성자를 만들어줍니다.
@@ -33,9 +40,14 @@ public class Product {
 
     @Column(nullable = false)
     private int myprice;
+    //회원의 아이디값 저장
+    @Column(nullable = false)
+    private Long userId;
 
     // 관심 상품 생성 시 이용합니다.
-    public Product(ProductRequestDto requestDto) {
+    public Product(ProductRequestDto requestDto, Long userId) {
+         // 관심상품을 등록한 회원 테이블 Id 저장
+        this.userId = userId;
         this.title = requestDto.getTitle();
         this.image = requestDto.getImage();
         this.link = requestDto.getLink();
